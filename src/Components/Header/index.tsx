b5abd99427b4f1c12219fa1ef05600b6a97a1752
SaaD036 <shaad036@gmail.com>
2024-12-21 12:51:31 +0600

    HBE-7:  Fix content design for generic headers

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -7,7 +7,7 @@ import styles from './styles.module.css';
 const Header = ({ components }: { components: React.ReactNode }) => {
     return (
         <div className={styles.expensesPageWrapper}>
-            <div>{components}</div>
+            <div className={styles.componentSection}>{components}</div>
             <div className={styles.profileSection}>
                 <HeaderProfile />
             </div>
